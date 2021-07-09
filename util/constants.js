e1e0aa9ace8718dbc9de76e46a0da8c19cc6e352
Sara Kanga <sarakanga600@gmail.com>
2021-07-08 23:29:13 -0500

    js files

---
+++ util/constants.js
@@ -0,0 +1,15 @@
+const TASK_STATUS = {
+	'To Do': 0,
+	'Completed': 1,
+};
+
+const LIST_STATUS = {
+	'Not Started': 0,
+	'In Progress': 1,
+	'Completed': 2,
+};
+
+module.exports = {
+	TASK_STATUS,
+	LIST_STATUS
+}
